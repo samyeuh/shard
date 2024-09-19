3ac37668a646495f62af12f99a6b2c3b2221f333
Enzo <mraktix@gmail.com>
2024-09-19 15:58:26 +0200

    To debug

--- Shard.EnzoSamy.Api/Controllers/BuildingController.cs
+++ Shard.EnzoSamy.Api/Controllers/BuildingController.cs
@@ -1,19 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
+using Shard.EnzoSamy.Api.Services;
+using Shard.EnzoSamy.Api.Utilities;
 
 namespace Shard.EnzoSamy.Api.Controllers;
 
+[Route("[controller]")]
+[ApiController]
 public class BuildingController(UserService userService) : ControllerBase
 {
     [HttpPost]
-    [Route("/{userId}/buildings")]
+    [Route("/users/{userId}/buildings")]
     public ActionResult<BuildingSpecification> BuildMineOnPlanet(string userId, [FromBody] BuildingSpecification buildingSpecification)
     {
-        var userBuilderUnit = userService.GetUnitsForUser(userId).FirstOrDefault(unit => unit.Type == "builder");
+        if (!ValidationUtils.IsValidUserId(userId)) return NotFound("Invalid user Id");
 
-        BuildingSpecification specification = new BuildingSpecification(buildingSpecification.Type, userBuilderUnit.Planet, userBuilderUnit.System );
-        
-        
-        
-        return specification;
+        var user = userService.FindUser(userId);
+        if (user == null) return NotFound($"User with ID {userId} not found.");
+
+        var userBuilderUnit = userService.GetUnitsForUser(userId)
+            .FirstOrDefault(unit => unit.Type == "builder");
+
+        if (userBuilderUnit == null) return BadRequest("Invalid builder ID.");
+
+        if (userBuilderUnit.Planet != null)
+        {
+            var specification = new BuildingSpecification(buildingSpecification.Type, userBuilderUnit.Planet, userBuilderUnit.System);
+            return specification;
+        }
+
+        return BadRequest("An error occured.");
     }
+
 }
