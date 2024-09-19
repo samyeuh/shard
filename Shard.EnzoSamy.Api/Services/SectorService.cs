3a86ae14355ffebd5191726ff2745eed3625c077
Enzo <mraktix@gmail.com>
2024-09-19 14:05:54 +0200

    Upt: Add error when system not found

--- Shard.EnzoSamy.Api/Services/SectorService.cs
+++ Shard.EnzoSamy.Api/Services/SectorService.cs
@@ -15,8 +15,14 @@ public class SectorService
         return _sectorSpecification.Systems;
     }
 
-    public SystemSpecification? GetOneSystem(string systemId)
+    public SystemSpecification GetOneSystem(string systemId)
     {
-        return _sectorSpecification.Systems.First(system => system.Name == systemId);
+        var system = _sectorSpecification.Systems.FirstOrDefault(s => s.Name == systemId);
+        if (system == null)
+        {
+            throw new InvalidOperationException($"System with ID {systemId} not found.");
+        }
+        return system;
     }
+
 }
